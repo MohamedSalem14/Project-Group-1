01a54766ff5c40592c117ecb8dda9f5c1f900eec
AmrRiad <119532551+AmrRiad@users.noreply.github.com>
2022-12-24 20:44:14 +0200

    edit note js

--- Javascript/main.js
+++ Javascript/main.js
@@ -6,7 +6,7 @@ var addBtn = document.querySelector(".addBtn");
 var noteHeader = document.getElementById("noteHeader");
 var noteBody = document.getElementById("noteBody");
 var noteTitle = document.querySelector(".noteTitle");
-var noteDetails = document.querySelector(".noteDetails")
+var noteDetails = document.querySelector(".noteDetails");
 var displayData = document.getElementById("displayData");
 var notes;
 
@@ -41,9 +41,9 @@ for(var i=0; i<notes.length; i++){
                 <h3 id="noteHeader" class="ms-3 mt-2 text-uppercase">${notes[i].title}</h3>
             </div>
             <div class="d-flex justify-content-between mx-2">
-                <textarea class="noteTextArea" id="noteBody" rows="6">${notes[i].body}</textarea>
+                <textarea disabled class="noteTextArea" id="noteBody" rows="6">${notes[i].body}</textarea>
             </div>
-            <a class="addBtn btn mb-1 text-center ">Edit Note</a>
+            <a id="editBtn" class="addBtn btn mb-1 text-center ">Edit Note</a>
             <a class="addBtn btn my-1 text-center">Remove Note</a>
     </div>
     `;
@@ -63,7 +63,7 @@ function addNotes (){
                 <h3 id="noteHeader" class="ms-3 mt-2 text-uppercase">${notes[i].title}</h3>
             </div>
             <div class="d-flex justify-content-between mx-2">
-                <textarea class="noteTextArea" id="noteBody" rows="6">${notes[i].body}</textarea>
+                <textarea disabled class="noteTextArea" id="noteBody" rows="6">${notes[i].body}</textarea>
             </div>
             <a class="addBtn btn mb-1 text-center ">Edit Note</a>
             <a class="addBtn btn my-1 text-center">Remove Note</a>
@@ -82,3 +82,12 @@ createNote.addEventListener("click", showNoteBox);
 closeIcon.addEventListener("click", closeNoteBox);
 document.addEventListener("keydown" ,closeByEscape);
 addBtn.addEventListener("click", addNotes);
+
+var addBtn = document.getElementById("editBtn");
+var textArea = document.querySelector(".noteTextArea")
+
+function editNoteFunction(){
+    textArea.removeAttribute("disabled")
+}
+
+addBtn.addEventListener("click",editNoteFunction)
